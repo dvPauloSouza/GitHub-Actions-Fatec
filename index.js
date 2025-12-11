d135c7fed3e1f2b18793e7e2ae78fdf1e0e17713
dvPauloSouza <paulobianso@gmail.com>
2025-12-11 18:55:39 -0300

    integrando CI entre GitHub e Azure

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, GitHub Actions");
+console.log("Olá, mundo! Este é um exemplo de intregração entre GitHub Action e Azure App Service!");
+
+const express = require('express');
+const app = express();
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.send(`<h1> Deploy feito com sucesso!</h1>
+        <p>Este app foi publicado automaticamente no <b> Azure App Service</b> via
+        <b>GitHub Actions</b>. </p>
+        <p>Exemplo criado por <b>Paulo Souza</b>.</p>
+        `);
+});
+
+// Porta dinâmica fornecida pelo Azure ou 3000 localmente
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
